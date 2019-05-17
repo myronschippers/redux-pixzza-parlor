2451e545267a5fdae392d935e6a3e5394d439b52
Myron <dubtwee@gmail.com>
2019-05-17 16:26:28 -0500

    display information from all steps in the order process on the checkout page

--- src/components/OrderCheckoutPage/OrderCheckoutPage.js
+++ src/components/OrderCheckoutPage/OrderCheckoutPage.js
@@ -9,12 +9,69 @@ class OrderCheckoutPage extends Component {
     }
 
     render() {
+        let typeText = '';
+        let totalCost = 0;
+        const {
+            name,
+            streetAddress,
+            city,
+            zip,
+        } = this.props.reduxState.customerReducer;
+
+        if (this.props.reduxState.typeReducer === 'delivery') {
+            typeText = 'For Delivery';
+        } else if (this.props.reduxState.typeReducer === 'pickup') {
+            typeText = 'For Pickup';
+        }
+
+        const pizzaOrderDetails = this.props.reduxState.pizzaOrderReducer.map((pizzaItem, pizzaIndex) => {
+            totalCost += parseFloat(pizzaItem.price)
+            return (
+                <tr key={pizzaIndex}>
+                    <td>
+                        {pizzaItem.name}
+                    </td>
+                    <td>
+                        ${pizzaItem.price}
+                    </td>
+                </tr>
+            );
+        });
+
         return (
             <div>
                 <h2>Step 3: Checkout</h2>
 
                 <div>
-                    BODY CONTENT
+                    <div>
+                        <div>
+                            {name}<br />
+                            {streetAddress}<br />
+                            {city}<br />
+                            {zip}
+                        </div>
+                        <div>
+                            {typeText}
+                        </div>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>
+                                    Name
+                                </th>
+                                <th>
+                                    Cost
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {pizzaOrderDetails}
+                        </tbody>
+                    </table>
+                    <div>
+                        Total: ${totalCost}
+                    </div>
                 </div>
                 <div>
                     <button onClick={this.clickCheckout}>CHECKOUT</button>
