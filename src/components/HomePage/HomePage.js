fb8e3e66bd861b42039cefeea84cf9cf06d6943a
Myron <dubtwee@gmail.com>
2019-05-18 23:51:37 -0500

    adjusting the home page styling to leverage bulma

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapReduxStateToProps from '../../modules/mapReduxStateToProps';
 
-// Material UI
-import Button from '@material-ui/core/Button';
 // CSS
 import './pizzaHero.css';
 
@@ -18,7 +16,12 @@ class HomePage extends Component {
                 <div className="pizzaHero">
                     <div className="pizzaHero-body">
                         <p className="pizzaHero-body-title">Get Your Slice Today</p>
-                        <Button variant="contained" color="primary" onClick={this.clickStartOrder}>Start Order</Button>
+                        <button
+                            className="button is-medium is-link"
+                            onClick={this.clickStartOrder}
+                        >
+                            Start Order
+                        </button>
                     </div>
                 </div>
             </div>
